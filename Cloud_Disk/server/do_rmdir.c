f05982c151f1f49d0aa479851ce3bd2a61b62663
zryk <130990107@qq.com>
2024-06-19 16:51:34 +0800

    do_mkdir.c

--- Cloud_Disk/server/do_rmdir.c
+++ Cloud_Disk/server/do_rmdir.c
@@ -2,5 +2,16 @@
 
 void rmdirCommand(task_t * task)
 {
-    printf("execute rmdir command.\n");
+    printf("execute mkdir command.\n");
+    train_t t;
+    memset(&t, 0, sizeof(t));
+    int err = rmdir(task->data);
+
+    if(err) {
+        strcpy(t.buff, "rmdir failed.\n");
+        sendn(task->peerfd, &t, strlen(task->data));
+        return;
+    }
+    strcpy(t.buff, "rmdir command succeed.\n");
+    sendn(task->peerfd, &t, strlen(task->data));
 }
