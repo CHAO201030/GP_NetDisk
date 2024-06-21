442b75172d14302e4473614fbdc08c3cad888b39
CHAO201030 <2227685463@qq.com>
2024-06-21 15:24:33 +0800

    CHAO: 24.06.21 15:23 Fix bug and optimize sql_do_cd func

--- Net_Disk_v1/server/route_server/src/do_cd.c
+++ Net_Disk_v1/server/route_server/src/do_cd.c
@@ -7,19 +7,14 @@ extern int log_fd;
 
 void do_cd(client_t *client, char *cmd)
 {
-    /*
-        1. 支持cd到当前目录的子目录 或者到上一级目录
-        2. cd后要把路径拼接到当前client的 path[128] 路径中
-    */
-
     char *target_dir = strtok(cmd, " ");
     target_dir = strtok(NULL, " ");
 
     train_t server_msg = {0};
     server_msg.state = CMD_CD;
 
-    if(sql_do_cd(client->uid, &client->code, &client->pre_code, target_dir) == 0)
-    {
+    if(sql_do_cd(client, target_dir) == 0)
+    { 
         if(strcmp(target_dir, "..") == 0)
         {
             int path_len = strlen(client->path);
@@ -66,7 +61,7 @@ void do_cd(client_t *client, char *cmd)
     return;
 }
 
-int sql_do_cd(int uid, int *code, int *pre_code, const char *target_dir)
+int sql_do_cd(client_t *client, const char *target_dir)
 {
     MYSQL_RES *sql_res = NULL;
     MYSQL_ROW sql_row;
@@ -78,57 +73,52 @@ int sql_do_cd(int uid, int *code, int *pre_code, const char *target_dir)
     if(strcmp(target_dir, "..") == 0)
     {
         // 去上一级目录
-        if(*code == 0)
+        if(client->code == 0)
         {
-            // printf("分类1\n");
             // 当前为根 不能再向上了 path : ~
             return -1;
         }
         else
         {
-            // 当前不是根 并且上一级是根 path : ~/dir1 --> ~/
-            if(*pre_code == 0)
+            // 当前不是根 上一级是根 path : ~/dir1 --> ~/
+            if(client->pre_code == 0)
             {
-                // printf("分类2\n");
-                *pre_code = -1;
-                *code = 0;
+                client->pre_code = -1;
+                client->code = 0;
+
                 return 0;
             }
             else
             {
                 // 当前不是根 上一级也不是根需要查数据库获得code信息 path : ~/dir1/dir2 --> ~/dir1
-                // printf("分类3\n");
                 sprintf(query, 
-                        "SELECT code, pre_code FROM VFS WHERE owner_uid = %d AND code = %d",
-                        uid, *pre_code);
+                        "SELECT code, pre_code FROM VFS WHERE owner_uid = %d AND code = %d AND file_type = 'd'",
+                        client->uid, client->pre_code);
                 classify = 3;
             }
 
         }
-
     }
     else
     {
         // 去下一级目录
-        if(*code == 0)
+        if(client->code == 0)
         {
             // 当前为根 path : ~/ --> ~/dir1
-            // printf("分类4\n");
-            *pre_code = 0;
+            client->pre_code = 0;
             sprintf(query,
                     "SELECT code FROM VFS WHERE "
-                    "owner_uid = %d AND file_name = '%s' AND pre_code < 0",
-                    uid, target_dir);
+                    "owner_uid = %d AND file_name = '%s' AND pre_code < 0 AND file_type = 'd'",
+                    client->uid, target_dir);
             classify = 4;
         }
         else
         {
             // 当前不是根
-            // printf("分类5\n");
             sprintf(query,
                     "SELECT code, pre_code FROM VFS WHERE "
-                    "owner_uid = %d AND pre_code = %d AND file_name = '%s'",
-                    uid, *code, target_dir);
+                    "owner_uid = %d AND pre_code = %d AND file_name = '%s' AND file_type = 'd'",
+                    client->uid, client->code, target_dir);
             classify = 5;
         }
     }
@@ -150,12 +140,11 @@ int sql_do_cd(int uid, int *code, int *pre_code, const char *target_dir)
                 {
                     // 当前不是根 上一级也不是根需要查数据库获得code信息
                     // path : ~/dir1/dir2 --> ~/dir1
-                    *code = atoi(sql_row[0]);
-                    *pre_code = atoi(sql_row[1]);
-                    // printf("code = %d, pre_code = %d\n",*code, *pre_code);
+                    client->code = atoi(sql_row[0]);
+                    client->pre_code = atoi(sql_row[1]);
 
-                    // 这个目录是根目录的直接孩子
-                    if(*pre_code == -1)*pre_code = 0;
+                    // 这个目录是根目录的直接子目录
+                    if(client->pre_code == -1)client->pre_code = 0;
                     
                     break;
                 }
@@ -163,14 +152,14 @@ int sql_do_cd(int uid, int *code, int *pre_code, const char *target_dir)
                 {
                     // 当前为根
                     // path : ~/ --> ~/dir1
-                    *code = atoi(sql_row[0]);
+                    client->code = atoi(sql_row[0]);
                     break;
                 }
                 case 5:
                 {
                     // 当前不是根
-                    *code = atoi(sql_row[0]);
-                    *pre_code = atoi(sql_row[1]);
+                    client->code = atoi(sql_row[0]);
+                    client->pre_code = atoi(sql_row[1]);
                     break;
                 }
                 }
@@ -187,4 +176,5 @@ int sql_do_cd(int uid, int *code, int *pre_code, const char *target_dir)
     mysql_free_result(sql_res);
 
     return 0;
+
 }
